3755b5b8d06b0bdb3ce235cee830d52d76d3306e
e <wxd1@live.com>
2017-11-30 15:50:52 +0800

    1.0.7(9) release 版本

--- app/src/main/java/com/wongxd/absolutedomain/base/kotin/extension/ImageView.kt
+++ app/src/main/java/com/wongxd/absolutedomain/base/kotin/extension/ImageView.kt
@@ -8,7 +8,23 @@ import com.wongxd.absolutedomain.R
  * Created by wxd1 on 2017/6/30.
  */
 
-fun ImageView.loadImg(imgPath: Any) {
+fun ImageView.loadImg(imgPath: String) {
+//    doAsync {
+//        val bytes = URL(imgPath).readBytes()
+//        uiThread {
+//            Glide.with(context.applicationContext).load(bytes)
+//                    .placeholder(R.drawable.placeholder)
+//                    .error(R.drawable.error)
+//                    .crossFade(500)
+//                    .into(it)
+//        }
+//    }
+
+    loadImgByPath(imgPath)
+
+}
+
+fun ImageView.loadImgByPath(imgPath: String) {
     Glide.with(context.applicationContext).load(imgPath)
             .placeholder(R.drawable.placeholder)
             .error(R.drawable.error)
